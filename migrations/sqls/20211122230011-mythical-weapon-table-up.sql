f95a28e23275c45d8c09fa922d0eae6979ecbf63
David Rivera <drtaupier@gmail.com>
2021-11-30 17:00:38 -0500

    I removed the first migration

--- migrations/sqls/20211122230011-mythical-weapon-table-up.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE users (name VARCHAR (100), lastname VARCHAR(100), username VARCHAR(100), password VARCHAR(100), role VARCHAR(20), id SERIAL PRIMARY KEY);
-
-CREATE TABLE products(product_name VARCHAR(100), price int () );
