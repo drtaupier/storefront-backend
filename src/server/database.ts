a30fdb1ccb34b03b06342835fcda566ec5582da0
David Rivera <drtaupier@gmail.com>
2021-11-30 14:10:07 -0500

    database.ts: I configured my database to connection to database

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,36 +1,21 @@
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
-
 dotenv.config();
 
 const {
 	POSTGRES_HOST,
 	POSTGRES_DB,
+	POSTGRES_TEST_DB,
 	POSTGRES_USER,
 	POSTGRES_PASSWORD,
-	POSTGRES_TEST_DB,
 	ENV,
 } = process.env;
 
-let client: Pool = new Pool();
-console.log(ENV);
-
-if (ENV === 'test') {
-	client = new Pool({
-		host: POSTGRES_HOST,
-		database: POSTGRES_DB,
-		user: POSTGRES_USER,
-		password: POSTGRES_PASSWORD,
-	});
-}
-
-if (ENV === 'dev') {
-	client = new Pool({
-		host: POSTGRES_HOST,
-		database: POSTGRES_TEST_DB,
-		user: POSTGRES_USER,
-		password: POSTGRES_PASSWORD,
-	});
-}
+const client: Pool = new Pool({
+	host: POSTGRES_HOST,
+	database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
+	user: POSTGRES_USER,
+	password: POSTGRES_PASSWORD,
+});
 
 export default client;
