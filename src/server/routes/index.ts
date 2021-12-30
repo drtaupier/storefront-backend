51a7e310386c680522abc21cb21ffeec491b3d81
David Rivera <drtaupier@gmail.com>
2021-12-30 12:28:11 -0500

    I removed the routes file

--- src/server/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { User, UserStore } from '../models/user';
-const routes = express.Router();
-
-routes.get('/', (req, res) => {
-	res.send('Hello World!');
-});
