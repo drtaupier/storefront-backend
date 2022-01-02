dcfd6de5f93d63930a953bc74f976588795ea598
David Rivera <drtaupier@gmail.com>
2022-01-02 16:02:11 -0500

    orders sql: I changed quantity type and add default 1

--- migrations/sqls/20211230043619-orders-up.sql
+++ migrations/sqls/20211230043619-orders-up.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS orders;
 DROP SEQUENCE IF EXISTS orders_orders_id_seq;
 CREATE TABLE orders(orders_id SERIAL PRIMARY KEY,
 product_id INTEGER,
-quantity INTEGER NOT NULL,
+quantity SMALLINT DEFAULT 1,
 status_id INTEGER,
 user_id INTEGER,
 FOREIGN KEY (product_id) REFERENCES products(product_id),
