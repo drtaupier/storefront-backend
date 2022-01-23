b12884397e496673eac32ab98250c21dbc9ceb4f
David Rivera <drtaupier@gmail.com>
2022-01-23 11:05:08 -0500

    I am doing the user test

--- src/server/models/tests/usersSpec.ts
+++ src/server/models/tests/usersSpec.ts
@@ -1,50 +1,91 @@
+import express, { Request, response, Response } from 'express';
 import { User, UserStore } from '../user';
-import supertest from 'supertest';
-import app from '../../server';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 dotenv.config();
-
 const store = new UserStore();
-const request = supertest(app);
 
 describe('User Model', () => {
-	it('Should have an index method', () => {
-		expect(store.index).toBeDefined();
+	describe('Test method exists', () => {
+		it('Should have an index method', () => {
+			expect(store.index).toBeDefined();
+		});
+		it('Should have an show method', () => {
+			expect(store.show).toBeDefined();
+		});
+		it('Should have an create method', () => {
+			expect(store.create).toBeDefined();
+		});
+		it('Should have an delete method', () => {
+			expect(store.delete).toBeDefined();
+		});
+		it('Should have an authentic method', () => {
+			expect(store.authenticate).toBeDefined();
+		});
 	});
-	it('Should have an show method', () => {
-		expect(store.show).toBeDefined();
-	});
-	it('Should have an create method', () => {
-		expect(store.create).toBeDefined();
-	});
-	it('Should have an delete method', () => {
-		expect(store.delete).toBeDefined();
-	});
-	it('Should have an authentic method', () => {
-		expect(store.authenticate).toBeDefined();
-	});
-});
 
-describe('Users endpoints testing', () => {
-	let token: string;
-	it('posts to /users endpoint, create a new user', async () => {
-		const response = await request
-			.post('/users/register')
-			.set({ 'API-Key': 'foobar', Accept: 'application/json' })
-			.send({
-				firstname: 'David',
-				lastname: 'Rivera',
-				username: 'drtaupier_test',
-				password: process.env.POSTGRES_TEST_DB as string,
+	describe('Test user model logic', () => {
+		const user = {
+			firstname: 'David',
+			lastname: 'Rivera',
+			username: 'drtaupier_test1',
+			password: 'password123',
+			role_id: 1,
+		} as User;
+
+		beforeAll(async () => {
+			const response = await store.authenticate(
+				user.username,
+				user.password as string
+			);
+		});
+		afterAll(async () => {
+			const result = await store.delete('1');
+		});
+
+		it('Create method should add a user', async () => {
+			const result = await store.create({
+				firstname: 'Flor',
+				lastname: 'Lopez',
+				username: 'flopez_test',
+				password: '$2b$10$kDtVwsXLLzEVmdH/HE9XEeKW8MRKNpcQwdJeCXgBwRvrlePb3SyB6',
 				role_id: 1,
 			});
-		token = 'Pass ' + response.body;
-		expect(response.status).toEqual(200);
-	});
-
-	it('Get "/users" get all the users', async () => {
-		const response = await request.get('/users').set('Authorization', token);
-		expect(response.status).toEqual(200);
+			expect(result).toEqual({
+				user_id: 2,
+				firstname: 'Flor',
+				lastname: 'Lopez',
+				username: 'flopez_test',
+				password: 'password123',
+				role_id: 1,
+			});
+		});
+		// it('Authenticate method should return the authenticate user', async () => {
+		// 	const token = await store.authenticate(username, password);
+		// 	expect(response.status).toEqual(200);
+		// });
 	});
 });
+
+// describe('Users endpoints testing', () => {
+// 	let token: string;
+// 	it('posts to /users endpoint, create a new user', async () => {
+// 		const response = await request
+// 			.post('/users/register')
+// 			.set({ 'API-Key': 'foobar', Accept: 'application/json' })
+// 			.send({
+// 				firstname: 'Flor',
+// 				lastname: 'Lopez',
+// 				username: 'flopez_test',
+// 				password: process.env.POSTGRES_TEST_DB as string,
+// 				role_id: 1,
+// 			});
+// 		token = 'Pass ' + response.body;
+// 		expect(response.status).toEqual(200);
+// 	});
+
+// 	it('Get "/users" get all the users', async () => {
+// 		const response = await request.get('/users').set('Authorization', token);
+// 		expect(response.status).toEqual(200);
+// 	});
+// });
