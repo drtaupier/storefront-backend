512758ad40702a148b64bc36b38c52029b05320f
David Rivera <drtaupier@gmail.com>
2022-01-26 00:43:55 -0500

    I deleted product_id and quantity from sql

--- migrations/sqls/20211230043619-orders-up.sql
+++ migrations/sqls/20211230043619-orders-up.sql
@@ -1,10 +1,7 @@
 DROP TABLE IF EXISTS orders;
 DROP SEQUENCE IF EXISTS orders_orders_id_seq;
 CREATE TABLE orders(orders_id SERIAL PRIMARY KEY,
-product_id INTEGER,
-quantity SMALLINT DEFAULT 1,
 status_id INTEGER,
 user_id INTEGER,
-FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (status_id) REFERENCES order_status(status_id) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE);
+FOREIGN KEY (status_id) REFERENCES order_status(status_id) ON DELETE CASCADE,
+FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE);
