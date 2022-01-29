c71b08857dbe56d02c91c8bc307ae348476db9db
David Rivera <drtaupier@gmail.com>
2022-01-29 16:45:43 -0500

    I am doing the User Model

--- src/server/models/tests/usersSpec.ts
+++ src/server/models/tests/usersSpec.ts
@@ -3,6 +3,9 @@ import supertest from 'supertest';
 const store = new UserStore();
 import app from '../../server';
 const request = supertest(app);
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 
 describe('User Model', () => {
 	describe('Test method exists', () => {
@@ -25,7 +28,8 @@ describe('User Model', () => {
 
 	describe('Test user model logic', () => {
 		let token: string;
-		const user = {
+
+		const drtaupier = {
 			firstname: 'David',
 			lastname: 'Rivera',
 			username: 'drtaupier_test',
@@ -33,8 +37,27 @@ describe('User Model', () => {
 			role_id: 1,
 		} as User;
 
+		const jpinedo = {
+			firstname: 'Jackie',
+			lastname: 'Pinedo',
+			username: 'jpinedo_test',
+			password: 'password123',
+			role_id: 2,
+		} as User;
+
+		const psw = async (user: User) => {
+			const firstname = user.firstname;
+			const lastname = user.lastname;
+			const username = user.username;
+			const password = user.password;
+			const role_id = user.role_id;
+
+			const hash = await store.authenticate(user.username, user.password);
+			return hash;
+		};
+
 		beforeAll(async () => {
-			const createUser = await store.create(user);
+			const createUser = await store.create(drtaupier);
 			const response = await request
 				.post('/user/login')
 				.send({
@@ -49,18 +72,15 @@ describe('User Model', () => {
 		});
 
 		it('Create method should add a user', async () => {
-			const result = await store.create({
+			const result = await request.post('/users/register').send({
 				firstname: 'Flor',
 				lastname: 'Lopez',
 				username: 'flopez_test',
 				password: 'password123',
 				role_id: 1,
-			} as User);
-			expect(result.user_id).toBe(4);
-			expect(result.firstname).toBe('Flor');
-			expect(result.lastname).toBe('Lopez');
-			expect(result.username).toBe('flopez_test');
-			expect(result.role_id).toBe(1);
+			});
+
+			expect(result.status).toEqual(201);
 		});
 		it('This method get all users from DB', async () => {
 			const response = await request.get('/users').set('Authorization', token);
@@ -73,6 +93,46 @@ describe('User Model', () => {
 		it('This method delete one user from DB', async () => {
 			const response = await request.delete('/users/4').set('Authorization', token);
 			expect(response.status).toBe(200);
+		});
+
+		describe('Users handlers', () => {
+			it('This method should create a user', async () => {
+				const result = await store.create({
+					firstname: 'Jackie',
+					lastname: 'Pinedo',
+					username: 'jpinedo_test',
+					password: 'password123',
+					role_id: 2,
+				});
+				const hash = await store.authenticate(jpinedo.username, jpinedo.password);
+				expect(result).toEqual({
+					firstname: 'Jackie',
+					lastname: 'Pinedo',
+					username: 'jpinedo_test',
+					password: hash,
+					role_id: 2,
+				});
+			});
+			it('This method should return all users', async () => {
+				const result = await store.index();
+				const hash: string = await store.authenticate(
+					drtaupier.username,
+					drtaupier.password
+				);
+				const hash1: string = await store.authenticate(
+					jpinedo.username,
+					jpinedo.password
+				);
+				expect(result).toEqual(hash, hash1);
+			});
+			it('This method should return a specific user', async () => {
+				const result = await store.show('5');
+				expect(result.user_id).toBe(5);
+				expect(result.firstname).toBe('Jackie');
+				expect(result.lastname).toBe('Pinedo');
+				expect(result.username).toBe('jpinedo_test');
+				expect(result.role_id).toBe(2);
+			});
 		});
 	});
 });
