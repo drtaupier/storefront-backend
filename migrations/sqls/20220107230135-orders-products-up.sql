a6f201110b5a593b8e530bc17204c751df72ef72
David Rivera <drtaupier@gmail.com>
2022-02-09 17:24:56 -0500

    I changed the type to orders_id and product_id

--- migrations/sqls/20220107230135-orders-products-up.sql
+++ migrations/sqls/20220107230135-orders-products-up.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS orders_products;
 DROP SEQUENCE IF EXISTS orders_products_orders_products_id_seq;
 CREATE TABLE orders_products(orders_products_id SERIAL PRIMARY KEY,
 quantity INTEGER DEFAULT 1,
-orders_id INTEGER REFERENCES orders(orders_id) ON DELETE CASCADE,
-product_id INTEGER REFERENCES products(product_id) ON DELETE RESTRICT);
+orders_id bigint REFERENCES orders(orders_id),
+product_id bigint REFERENCES products(product_id));
