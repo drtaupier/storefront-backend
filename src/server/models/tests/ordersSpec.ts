f760ccea75e6e3fac8b4d04349edf080bbf76c49
David Rivera <drtaupier@gmail.com>
2022-02-11 00:52:48 -0500

    I added products and orders_products in my orders test

--- src/server/models/tests/ordersSpec.ts
+++ src/server/models/tests/ordersSpec.ts
@@ -1,20 +1,44 @@
 import { User, UserStore } from '../user';
 import { Order, OrderStore } from '../orders';
+import { Product, ProductStore } from '../products';
+import { Orders_products, Orders_productsStore } from '../orders_products';
 import supertest from 'supertest';
 import app from '../../server';
 const userStore = new UserStore();
 const store = new OrderStore();
+const productStore = new ProductStore();
+const orderProductStore = new Orders_productsStore();
 const request = supertest(app);
 
 describe('Orders model', () => {
 	let token: string;
 	const user = {
+		//users_id: 2,
 		firstname: 'David',
 		lastname: 'Rivera',
 		username: 'drtaupier_test',
 		password: 'password123',
 		role_id: 1,
 	} as User;
+
+	const order = {
+		//order_id : 2,
+		status_id: 1,
+		user_id: 1,
+	} as Order;
+
+	const product = {
+		//product_id:2,
+		name: 'Olive Sandwich',
+		price: 6,
+	} as Product;
+
+	const orderProduct = {
+		//orders_products_id: 3,
+		quantity: 1,
+		orders_id: 2,
+		product_id: 2,
+	} as Orders_products;
 
 	beforeAll(async () => {
 		const createUser = await userStore.create(user);
@@ -26,10 +50,15 @@ describe('Orders model', () => {
 			})
 			.set('Accept', 'application/json');
 		token = 'Bearer ' + auth.body;
+		const createOrder = store.create(order);
+		const createProduct = productStore.create(product);
+		const createOrderProduct = orderProductStore.create(orderProduct);
 	});
 	afterAll(async () => {
-		const deleteUser = await userStore.delete('1');
-		const deleteOrder = await store.delete('2');
+		await userStore.delete('2');
+		await productStore.delete('2');
+		//await store.delete('2');
+		await orderProductStore.delete('3');
 	});
 
 	describe('Test method exists', () => {
@@ -46,14 +75,11 @@ describe('Orders model', () => {
 			expect(store.delete).toBeDefined();
 		});
 	});
-	describe('Test orders EndPoints', () => {
-		it('Create method should add a order', async () => {
+	describe('Test orders Models', () => {
+		it('Create method should add an order', async () => {
 			const result = await request
-				.post('/orders')
-				.send({
-					status_id: 1,
-					user_id: 1,
-				})
+				.post('/orders/2')
+				.send(order)
 				.set('Authorization', token);
 			expect(result.status).toEqual(201);
 		});
@@ -62,21 +88,16 @@ describe('Orders model', () => {
 			expect(result.status).toEqual(200);
 		});
 		it('This method get one specific order', async () => {
-			const result = await request.get('/orders/1').set('Authorization', token);
+			const result = await request.get('/orders/2	').set('Authorization', token);
 			expect(result.status).toEqual(200);
 		});
 	});
 
 	describe('Test handlers', () => {
-		const order = {
-			status_id: 1,
-			user_id: 1,
-		} as Order;
-
-		it('create mehod should add a order', async () => {
+		it('create method should add a order', async () => {
 			const result = await store.create(order);
 			expect(result).toEqual({
-				orders_id: 2,
+				orders_id: 4,
 				status_id: 1,
 				user_id: 1,
 			});
@@ -85,12 +106,17 @@ describe('Orders model', () => {
 			const result = await store.index();
 			expect(result).toEqual([
 				{
-					orders_id: 1,
+					orders_id: 2,
 					status_id: 1,
 					user_id: 1,
 				},
 				{
-					orders_id: 2,
+					orders_id: 3,
+					status_id: 1,
+					user_id: 1,
+				},
+				{
+					orders_id: 4,
 					status_id: 1,
 					user_id: 1,
 				},
@@ -105,7 +131,7 @@ describe('Orders model', () => {
 			});
 		});
 		it('delete method should remove the order', async () => {
-			store.delete('1');
+			store.delete('2');
 			const result = await store.index();
 			expect(result).toEqual([
 				{
