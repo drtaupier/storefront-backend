2deded22d237f87806c856486116e465bea000a4
David Rivera <drtaupier@gmail.com>
2022-02-12 12:36:31 -0500

    I changed the type to INTEGER: orders_id and product_id

--- migrations/sqls/20220107230135-orders-products-up.sql
+++ migrations/sqls/20220107230135-orders-products-up.sql
@@ -2,5 +2,7 @@ DROP TABLE IF EXISTS orders_products;
 DROP SEQUENCE IF EXISTS orders_products_orders_products_id_seq;
 CREATE TABLE orders_products(orders_products_id SERIAL PRIMARY KEY,
 quantity INTEGER DEFAULT 1,
-orders_id bigint REFERENCES orders(orders_id),
-product_id bigint REFERENCES products(product_id));
+orders_id INTEGER,
+product_id INTEGER,
+FOREIGN KEY (orders_id) REFERENCES orders(orders_id),
+FOREIGN KEY (product_id) REFERENCES products(product_id));
