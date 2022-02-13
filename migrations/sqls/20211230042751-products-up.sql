e175f7feeb5a7a5ef4dd93b7fd447f175e327aff
David Rivera <drtaupier@gmail.com>
2022-02-13 11:51:48 -0500

    I added check for price

--- migrations/sqls/20211230042751-products-up.sql
+++ migrations/sqls/20211230042751-products-up.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS product;
 DROP SEQUENCE IF EXISTS product_product_id_seq;
-CREATE TABLE products(product_id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, price INTEGER NOT NULL);
+CREATE TABLE products(product_id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, price INTEGER CHECK(price >= 0) NOT NULL);
